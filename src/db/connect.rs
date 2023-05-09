c71420e8add3614b0dd9a7440c39d39eaa34ef94
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2023-05-09 14:15:56 -0300

    refac: use env variables

--- src/db/connect.rs
+++ src/db/connect.rs
@@ -1,14 +1,17 @@
+use dotenv::dotenv;
+use std::env;
 use std::sync::Arc;
 
 use http_problem::prelude::*;
 use tokio_postgres::Client;
 
 pub async fn connect() -> Result<Arc<Client>> {
-    let (client, connection) = tokio_postgres::connect(
-        "host=localhost user=postgres password=postgres dbname=users_actix",
-        tokio_postgres::NoTls,
-    )
-    .await.unwrap();
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL not set in .env file");
+
+    let (client, connection) = tokio_postgres::connect(&database_url, tokio_postgres::NoTls)
+        .await
+        .unwrap();
 
     tokio::spawn(async move { connection.await.unwrap() });
     Ok(Arc::new(client))
